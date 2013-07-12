7fa7bbb5be611848023f60dbe222b11b81c240ad
Jeff Poore <jefferylpoore@gmail.com>
2013-07-12 11:50:29 +0000

    read from index file

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = require('buffer');
+var buf1 = require('buffer');
 
 var app = express.createServer(express.logger());
 
@@ -10,16 +10,6 @@ var str = buf1.toString('utf8');
 app.get('/', function(request, response) {
     response.send(str);
 }); 
-//  response.send('Hello World this is Jeff!!');
-
-
-//    var buf1 = fs.readFile(index.html)
-//    var str = buf.toString('utf8')
-//    response.send(str)
-//
-//    var str = buf.toString('utf8' fs.readFile(index.html)) 
-//    respose.send(str)
-//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
