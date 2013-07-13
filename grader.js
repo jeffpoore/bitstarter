63b708930c014a931e9a89e38ffde1ea6d8e4832
Jeff Poore <jefferylpoore@gmail.com>
2013-07-13 00:14:29 +0000

    mods to read url

--- grader.js
+++ grader.js
@@ -5,7 +5,7 @@ Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
 References:
-
+i
  + cheerio
    - https://github.com/MatthewMueller/cheerio
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,28 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url') 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+    if (program.url) {
+       var chkurl = program.url;
+       restler.get(chkurl).on('complete', function(result){
+           $ = cheerio.load(result);
+           var checks = loadChecks(program.checks).sort();
+           var out = {};
+           for(var ii in checks) {
+               var present = $(checks[ii]).length > 0;
+               out[checks[ii]] = present;
+           }
+           var outJson = JSON.stringify( out, null, 4);      
+           console.log(outJson);
+             
+       });
+       var checkhtmlt 
+    }else{
+          var checkJson = checkHtmlFile(program.file, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+    } 
+}else {
     exports.checkHtmlFile = checkHtmlFile;
 }
